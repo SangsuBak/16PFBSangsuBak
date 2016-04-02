7fc3bbaf447ce0c9e1165c0f87b760af19988fa6
CAD Client <CAD Client>
2016-04-02 11:24:58 +0900

    modified ex09.py

--- ex09.py
+++ ex09.py
@@ -1,7 +1,8 @@
+# - coding: utf8 -*-
 # Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # \기호를 사용하면 백슬래쉬를 쓸수 있다.
 
 print "Here are the days: ", days
 print "Here are the months: ", months
@@ -12,3 +13,4 @@ With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 """
+# 코딩을 할 때, utf8을 사용하면 한글주석을 달면서 백슬래쉬도 같이 이용할 수 있다.
