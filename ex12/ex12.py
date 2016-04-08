e23ef1adff17cff9668edced012cc85dae71f662
CAD Client <CAD Client>
2016-04-08 14:18:05 +0900

    modified ex12.py

--- ex12/ex12.py
+++ ex12/ex12.py
@@ -1,6 +1,9 @@
+# -*- coding: utf8 -*-
+# http://learnpythonthehardway.org/book/ex12.html
 age = raw_input("How old are you? ")
 height =raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
 
 print "So, you're %r old, %r tall and %r heavy." % (
     age, height, weight)
+# git bash에서 pydoc이라고 입력을 한 뒤 명령어를 검색하면 그 명령어에 대한 도움말을 볼 수 있다.
